ea369bdb1a6fd9adc4bf077577202321f44dc5a5
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2023-02-22 18:30:45 +0300

    created comment model class

---
+++ src/main/java/com/tahafurkan/sandbox/blogapplication/entity/Comment.java
@@ -0,0 +1,24 @@
+package com.tahafurkan.sandbox.blogapplication.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String fullName;
+    private String email;
+    private String body;
+    
+}
