08b7a097299136bd389293b4ecce577ae68755a4
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2023-02-23 19:33:27 +0300

    Added BlogAPIExcepiton class

---
+++ src/main/java/com/tahafurkan/sandbox/blogapplication/exception/BlogAPIException.java
@@ -0,0 +1,23 @@
+package com.tahafurkan.sandbox.blogapplication.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BlogAPIException extends RuntimeException{
+
+    private HttpStatus httpStatus;
+    private String message;
+
+    public BlogAPIException(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
